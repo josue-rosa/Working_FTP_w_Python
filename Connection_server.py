6ef7b2295bbe3c1eb9a11c94735bd80be5f9232a
josue-rosa <josuecamponogara@hotmail.com>
2020-11-07 00:17:03 -0300

    update project

--- Connection_server.py
+++ Connection_server.py
@@ -1,19 +1,19 @@
 import ftplib
 import time
-import zipfile
+# import zipfile
 
-login = 'josue.rosa'
+login = 'josue'
 passwd = str(input('Informe a senha do FTP: ')).strip()
 dir_name = '/'
 
-conexao = ftplib.FTP('127.0.0.1')
+site = ftplib.FTP('127.0.0.1')
 print('Conectando...')
-conexao.login(login, passwd)
+site.login(login, passwd)
 time.sleep(1)
 print(f'Connection Successfuly \nLogado como: {login}')
 
-conexao.cwd(dir_name) # Mudar para o diretorio
-print(conexao.retrlines('LIST')) # Listar o conteudo do diretorio
+site.cwd(dir_name) # Mudar para o diretorio
+print(site.retrlines('LIST')) # Listar o conteudo do diretorio
 
-conexao.quit()
+site.quit()
 print('Desconectado do servidor \n')
